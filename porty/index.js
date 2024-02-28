4795dd19be75a63857df24aa0d04eb361502fae8
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-02-28 14:00:30 +0100

    Add files via upload

---
+++ porty/index.js
@@ -0,0 +1,21 @@
+
+
+
+function openGitHubProfile() {
+
+    var githubUrl = 'https://github.com/akintomiwa200';
+    
+    window.open(githubUrl, '_blank');
+    }
+    
+    document.getElementById('githubIcon').onclick = openGitHubProfile;
+    
+function openLinkedInProfile() {
+    
+    var linkedInUrl = 'https://www.linkedin.com/in/adedokun peter akintomiwa';
+    
+    window.open(linkedInUrl, '_blank');
+    }
+    
+    document.getElementById('linkedinIcon').onclick = openLinkedInProfile;
+    
